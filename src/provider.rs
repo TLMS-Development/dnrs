bafe07d4858a44792d39eae102f8d2cce9418ced
Torben Schweren <torben@schweren.dev>
2025-06-27 21:59:59 +0200

    add: Provider trait

--- src/provider.rs
+++ src/provider.rs
@@ -1,1 +1,36 @@
 pub mod nitrado;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Feature {
+    GetRecord,
+    GetRecords,
+    AddRecord,
+    UpdateRecord,
+    DeleteRecord,
+}
+
+pub trait Provider {
+    type GetRecordInput;
+    type GetRecordOutput;
+    type GetRecordsInput;
+    type GetRecordsOutput;
+    type AddRecordInput;
+    type AddRecordOutput;
+    type UpdateRecordInput;
+    type UpdateRecordOutput;
+    type DeleteRecordInput;
+    type DeleteRecordOutput;
+
+    fn get_provider_name() -> &'static str;
+
+    fn get_supported_features() -> Vec<Feature>;
+    fn is_feature_supported(feature: &Feature) -> bool {
+        Self::get_supported_features().contains(feature)
+    }
+
+    fn get_record(&self, input: &Self::GetRecordInput) -> &Self::GetRecordOutput;
+    fn get_records(&self, input: &Self::GetRecordsInput) -> &Self::GetRecordsOutput;
+    fn add_record(&self, input: &Self::AddRecordInput) -> &Self::AddRecordOutput;
+    fn update_record(&self, input: &Self::UpdateRecordInput) -> &Self::UpdateRecordOutput;
+    fn delete_record(&self, input: &Self::DeleteRecordInput) -> &Self::DeleteRecordOutput;
+}
